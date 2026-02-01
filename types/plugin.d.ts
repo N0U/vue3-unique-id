553d0ffc6fb9c6eddc115139f97b9d7503d54eb1
banana_Ai <petushkov.egor@gmail.com>
2026-02-01 23:00:21 +0300

    Fix import names

--- types/plugin.d.ts
+++ types/plugin.d.ts
@@ -2,8 +2,8 @@ import type { Plugin } from 'vue';
 
 type IdTable = Record<string, string>;
 
-declare const IdPlugin: Plugin;
-export default IdPlugin;
+declare const VueUniqueId: Plugin;
+export default VueUniqueId;
 
 declare module "vue" {
   interface ComponentCustomProperties {
